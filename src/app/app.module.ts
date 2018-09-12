0587666ecb7424ae812145e3414608e7d7e2064a
natrayan <natrayanp@gmail.com>
2018-09-12 23:56:54 +0800

    project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-
 import { AppComponent } from './app.component';
 import { ServicesModule } from './services/services.module';
 import { SharedModule } from './shared/shared.module';
